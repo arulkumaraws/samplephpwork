dc582e5270bd89709490ec2d788dd5a18145b802
arulkumaraws <36557022+arulkumaraws@users.noreply.github.com>
2018-02-17 10:28:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,19 @@
 <body>
 
 <h1>Hai This My Frist Php.app Dev<h1>
+  
+<html>
+<body>
 
+<?php
+ECHO "Hai Arul!<br>";
+echo "Hai Arul!<br>";
+EcHo "Hai Arul!<br>";
+?>
+
+</body>
+</html>
+  
 <?php
 echo "Version: 1.0";
 ?>
